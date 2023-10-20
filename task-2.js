b68ca76b6deec7334541e7477936080ddcc5ac79
Anastassia2312 <23122000na@gmail.com>
2023-10-20 14:56:30 +0300

    task-4

--- task-2.js
+++ task-2.js
@@ -1,4 +1,4 @@
-/*"use strict";
+"use strict";
 function formatMessage(message, maxLength) {
   const messageLength = message.length;
   const cutMessage = message.slice(0, maxLength);
@@ -14,4 +14,4 @@ console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula 
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));*/
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
